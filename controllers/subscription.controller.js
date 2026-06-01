0f983bbc27769c8ad795fb9891843779037897b0
Sajjad <sajjadqaderi00@gmail.com>
2026-06-01 14:32:50 +0200

    created a workflow that remind users about thier subscription date. Everything has been tested and worked perfectly. Next step is to send emails to users

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,12 +1,31 @@
 import Subscription from '../models/subscription.model.js';
+import { workflowClient } from '../config/upstash.js';
+import { SERVER_URL } from '../config/env.js';
 
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({ ...req.body, user: req.user._id });
 
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      retries: 0,
+    });
+
+    console.log(`Workflow run ID: ${workflowRunId}`);
+
     res
       .status(201)
-      .json({ success: true, message: 'Subscription created successfully', data: subscription });
+      .json({
+        success: true,
+        message: 'Subscription created successfully',
+        data: { subscription, workflowRunId },
+      });
   } catch (e) {
     next(e);
   }
